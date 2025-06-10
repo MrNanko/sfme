ca51ec87e45b825acc6d1a57fd5ead56affa2c53
Leamx <jiebutianxia@outlook.com>
2025-06-10 09:56:07 +0800

    refactor: remove legacy framework code

--- sfme/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# _*_ coding:utf-8 _*_
-
-# @Time    : 2022/5/3 07:35
-# @Author  : leamx
-# @File    : __init__.py
-# @Software: PyCharm
-
-__version__ = "0.0.1"
--- sfme/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# _*_ coding:utf-8 _*_
-
-# @Time    : 2022/5/3 07:48
-# @Author  : leamx
-# @File    : __init__.py
-# @Software: PyCharm
