28e8d9802559e6b6a57d9e4d6f6338c08c9661f0
MrNanko <jiebutianxia@outlook.com>
2026-01-16 15:45:20 +0800

    feat: add function to send media messages via Telegram Bot API

--- utils/bot_sender.py
+++ utils/bot_sender.py
@@ -7,8 +7,12 @@
 # @Software   : PyCharm
 # @Description:
 
+import logging
 import requests
 from ..config import config
+
+logger = logging.getLogger(__name__)
+
 
 def send_text_message(chat_id, text, reply_to_message_id=None):
 
@@ -29,4 +33,44 @@ def send_text_message(chat_id, text, reply_to_message_id=None):
         'reply_to_message_id': reply_to_message_id
     }
     response = requests.post(url, data=data)
+    return response.json()
+
+
+def send_media_message(chat_id, caption, file_path, media_type='photo'):
+    """使用 Bot API 发送媒体消息
+
+    Args:
+        chat_id: 目标聊天 ID
+        caption: 媒体说明文字
+        file_path: 本地媒体文件路径
+        media_type: 媒体类型 (photo, video, document, animation)
+    """
+    token = config.get('bot_token', None)
+
+    if not chat_id:
+        raise ValueError("Chat ID is required.")
+    if not token:
+        raise ValueError("Bot token is not configured.")
+
+    api_methods = {
+        'photo': 'sendPhoto',
+        'video': 'sendVideo',
+        'document': 'sendDocument',
+        'animation': 'sendAnimation',
+    }
+
+    method = api_methods.get(media_type, 'sendDocument')
+    url = f'https://api.telegram.org/bot{token}/{method}'
+
+    file_param = 'photo' if media_type == 'photo' else media_type
+
+    with open(file_path, 'rb') as f:
+        files = {file_param: f}
+        data = {
+            'chat_id': chat_id,
+            'caption': caption,
+            'parse_mode': 'HTML',
+        }
+        response = requests.post(url, data=data, files=files)
+
     return response.json()
